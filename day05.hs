326f5abadf4558474214351032a76e191f54a856
hnOsmium0001 <hn.osmium0001@gmail.com>
2023-10-07 20:51:45 -0700

    Day 5 part 1 in Haskell

--- day05.hs
+++ day05.hs
@@ -1,45 +1,86 @@
+import Data.Char
 import Data.Either
+import Data.List
+import Data.Tuple
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
 import qualified Data.Text.Read as T
 
-parseCrateSacks :: T.Text -> [[Char]]
-parseCrateSacks =
+dropIf pred xs = [x | x <- xs, not (pred x)]
+
+parseCrateStacks :: T.Text -> [[Char]]
+parseCrateStacks =
     map T.unpack
-  . T.transpose
+  . dropIf T.null
     -- Get rid of brackets and spaces
   . map (T.filter isUpper)
+  . T.transpose
   . T.lines
 
-parseInstruction :: T.Text -> (Int, Int, Int)
-parseInstruction str = case result of
-    Left _ -> error "malformed input"
-    Right ins -> ins
-    where result = do
-        Right (a, rest) <- T.decimal str
-        Right (b, rest') <- T.decimal rest
-        Right (c, rest'') <- T.decimal rest'
-        return (a, b, c)
+tuplify2 [x,y] = (x,y)
+tuplify3 [x,y,z] = (x,y,z)
+
+panicError :: Show a => Either a b -> b
+panicError (Left e) = error ("unexpected Left" ++ (show e))
+panicError (Right v) = v
 
 parseInstructions :: T.Text -> [(Int, Int, Int)]        
 parseInstructions =
-    map parseInstruction
-    -- "1 2 3"
-  . map (T.filter \x -> isNumber x || isSpace x)
-    -- "move 1 from 2 to 3"
+    map ( tuplify3
+        . map (fst . panicError . T.decimal)
+          -- ["1", "2", "3"]
+        . filter (not . T.null)
+          -- ["1", "", "2", "", "3"]
+        . T.splitOn (T.singleton ' ')
+          -- " 1  2  3"
+        . T.filter (\x -> isNumber x || isSpace x)
+          -- "move 1 from 2 to 3"
+        )
   . T.lines
 
-executeStep :: [[a]] -> (Int, Int, Int) -> [[a]]
-executeStep stack step = 
-    let (count, fromPile, toPile) = step,
-        splitAt count stack!!fromPile
-    --where 
+operateElements list n m func
+  | n < m = operate list n m func
+  | n > m = operate list m n (\a b -> swap (func b a))
+  | n == m = error "Cannot operate on the same element"
+  where operate list n m func =
+          let (front, rest) = splitAt n list
+              (middle, rest') = splitAt (m - n - 1) (tail rest)
+              back = tail rest'
+              (leftRes, rightRes) = func (head rest) (head rest')
+          in front ++ [leftRes] ++ middle ++ [rightRes] ++ back
 
-part1 inp = 0
+insSrc (_,x,_) = x
+insDst (_,_,x) = x
+insCount (x,_,_) = x
+
+applyIns :: [[Char]] -> (Int, Int, Int) -> [[Char]]
+applyIns stacks ins = operateElements stacks isrc idst handler
+  where count = insCount ins
+        -- Instruction indices are 1-indexed, convert to 0-index for operation on lists
+        isrc = insSrc ins - 1
+        idst = insDst ins - 1
+        handler fromPile toPile = (fromPile', toPile')
+          where fromPile' = drop count fromPile
+                toPile' = (reverse $ take count fromPile) ++ toPile
+
+applyInsList stacks insList = foldl applyIns stacks insList
+
+app (v1,v2) (f1,f2) = (f1 v1, f2 v2)
+
+preprocessInput inp = (crateStacks, instructions)
+  where [p1, p2] = T.splitOn (T.pack "\n\n") inp
+        crateStacks = (T.stripEnd . fst . T.breakOnEnd (T.pack "\n")) p1
+        instructions = p2
+
+extractTopCrates = map head
+
+part1 inp = extractTopCrates $ uncurry applyInsList $ app (preprocessInput inp) (parseCrateStacks, parseInstructions)
 
 part2 inp = 0
 
-main = do
-    lines <- (fmap T.lines . T.readFile) "inputs/day04.txt"
-    putStrLn $ "Part 1: " ++ (show $ part1 lines)
-    putStrLn $ "Part 2: " ++ (show $ part2 lines)
+parts filePath = do
+    inp <- T.readFile filePath
+    putStrLn $ "Part 1: " ++ (show $ part1 inp)
+    putStrLn $ "Part 2: " ++ (show $ part2 inp)
+
+main = parts "inputs/day05.txt"
